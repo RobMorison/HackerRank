d64e164fdaf20a0c2b658febd9b4b066cc143ac4
Sara Morison <rob.morison32@gmail.com>
2023-07-21 13:38:42 -0400

    solved 2nd python star

--- test.py
+++ test.py
@@ -36,7 +36,47 @@
 # for i in range (n):
 #     print (i+1,end='')
 
-n = 4
+# n = 4
 
-for i in range(n):
-    print(i * i)
+# for i in range(n):
+#     print(i * i)
+
+# An extra day is added to the calendar almost every four years as February 29, and the day is called a leap day. It corrects the calendar for the 
+# fact that our planet takes approximately 365.25 days to orbit the sun. A leap year contains a leap day.
+
+# In the Gregorian calendar, three conditions are used to identify leap years:
+
+# The year can be evenly divided by 4, is a leap year, unless:
+# The year can be evenly divided by 100, it is NOT a leap year, unless:
+# The year is also evenly divisible by 400. Then it is a leap year.
+# This means that in the Gregorian calendar, the years 2000 and 2400 are leap years, while 1800, 1900, 2100, 2200, 2300 and 2500 are NOT leap years. Source
+
+
+
+def is_leap(year):
+    leap = False
+
+    if year % 4 == 0:
+        
+        if year % 100 == 0:
+
+      
+            if year % 400 == 0:
+                leap = True
+
+            else:
+                return leap
+        
+        else:
+            return leap
+    
+    
+    else:
+        return leap
+
+
+
+    return leap
+
+is_leap(2100)
+    
