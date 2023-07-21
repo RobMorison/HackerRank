afb13d266399d89832a11a5b0ce7f7c1b12ed18b
Sara Morison <rob.morison32@gmail.com>
2023-07-21 09:40:04 -0400

    Added test python file

---
+++ test.py
@@ -0,0 +1,37 @@
+# def avg (arguments):
+#     total_sum = 0
+#     for number in arguments:
+#         total_sum += arguments
+#     return(total_sum)
+
+# avg(10)
+
+# n = 6
+
+# if n % 2 == 0 and n >1 and n <6:
+#     print('not weird')
+# elif n >1 and n <6:
+#     print('wierd')
+# else:
+#     print('number out of range select a number 2-5')
+
+# a = 2
+# b = 4
+# c = a - b
+
+# print (c)
+# print (b)
+
+# a = 3
+# b = 5
+
+# c = 3 // 5
+# d = 3 / 5
+
+# print(c)
+# print (d)
+
+n = 4
+
+for i in range (n):
+    print (i+1,end='')
