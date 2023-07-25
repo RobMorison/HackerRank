932b6d28b33beb4371f98ffcd6b05dce2cf60fa6
Sara Morison <rob.morison32@gmail.com>
2023-07-25 11:12:22 -0400

    Added CSI file + panda extension

---
+++ CSI.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('CSI_Data.csv')
+
+df.describe()
--- test.py
+++ test.py
@@ -53,30 +53,34 @@
 
 
 
-def is_leap(year):
-    leap = False
+# def is_leap(year):
+#     leap = False
 
-    if year % 4 == 0:
+#     if year % 4 == 0:
         
-        if year % 100 == 0:
+#         if year % 100 == 0:
 
       
-            if year % 400 == 0:
-                leap = True
+#             if year % 400 == 0:
+#                 leap = True
+#                 return leap
 
-            else:
-                return leap
+#             else:
+#                 return leap
         
-        else:
-            return leap
+#         else:
+#             return leap
     
     
-    else:
-        return leap
+#     else:
+#         return leap
+
+# is_leap(2100)
 
 
+s = 'hello openGeNus'
+t = s.title()
 
-    return leap
+print(t)
 
-is_leap(2100)
     
