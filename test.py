74a24e15fec780de11cbef282248633f378e4f6c
Sara Morison <rob.morison32@gmail.com>
2023-08-03 12:10:15 -0400

    SQL problems

--- test.py
+++ test.py
@@ -93,18 +93,22 @@
 # else:
 #     print('nope')
 
-N = []
+# N = []
 
-N.insert(0, 5)
-N.insert(1, 10)
-N.insert(0, 6)
-print(N)
-N.remove(6)
-N.append(9)
-N.append(1)
-N.sort()
-print(N)
-N.pop(-1)
-N.reverse()
-print(N)
+# N.insert(0, 5)
+# N.insert(1, 10)
+# N.insert(0, 6)
+# print(N)
+# N.remove(6)
+# N.append(9)
+# N.append(1)
+# N.sort()
+# print(N)
+# N.pop(-1)
+# N.reverse()
+# print(N)
+
+SELECT NAME FROM STUDENTS WHERE MARKS > 75;
+
+SELECT (months * salary) as total_earnings FROM EMPLOYEES;
  
