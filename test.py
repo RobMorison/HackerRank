f3175a700f1bef3643aae60082ab6039f9211f87
Sara Morison <rob.morison32@gmail.com>
2023-08-06 19:45:13 -0400

    SQL Weather Station 9 Solve

--- test.py
+++ test.py
@@ -136,3 +136,6 @@ Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Yo
 
 Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) as both their first and last characters. Your result cannot contain duplicates.
     SELECT DISTINCT CITY FROM STATION WHERE LEFT (CITY,1) IN ('A','E','I','O','U') AND RIGHT (CITY,1) IN ('a','e','i','o','u');
+
+Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
+    SELECT DISTINCT CITY FROM STATION WHERE LEFT (CITY,1) NOT IN ('A','E','I','O','U');
