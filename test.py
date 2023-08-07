7c2af62ed5cf1ecbfd3c063578116aa742adc653
Sara Morison <rob.morison32@gmail.com>
2023-08-07 14:46:17 -0400

    SQL weather station 11

--- test.py
+++ test.py
@@ -147,3 +147,6 @@ If there is more than one smallest or largest city, choose the one that comes fi
 
 Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.
     SELECT DISTINCT CITY FROM STATION WHERE RIGHT (CITY,1) NOT IN ('a','e','i','o','u');
+
+Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
+    SELECT DISTINCT CITY FROM STATION WHERE LEFT (CITY, 1) NOT IN ('A', 'E', 'I', 'O', 'U') OR RIGHT (CITY, 1) NOT IN ('a', 'e', 'i', 'o', 'u');
