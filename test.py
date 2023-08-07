b7116dfec9d696ad1254c382e11ed65ae5549332
Sara Morison <rob.morison32@gmail.com>
2023-08-07 14:37:09 -0400

    SQL weather station 9 solution

--- test.py
+++ test.py
@@ -139,3 +139,8 @@ Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, a
 
 Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
     SELECT DISTINCT CITY FROM STATION WHERE LEFT (CITY,1) NOT IN ('A','E','I','O','U');
+
+Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths (i.e.: number of characters in the name). 
+If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically.
+    SELECT CITY, LENGTH(CITY) FROM STATION ORDER BY LENGTH(CITY), CITY LIMIT 1;
+    SELECT CITY, LENGTH(CITY) FROM STATION ORDER BY LENGTH(CITY) DESC LIMIT 1;
