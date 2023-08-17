79141e6b86d9f09cd4c9e545181ea98262012c24
Sara Morison <rob.morison32@gmail.com>
2023-08-17 13:41:14 -0400

    SQL Average

--- test.py
+++ test.py
@@ -166,3 +166,6 @@ Query a count of the number of cities in CITY having a Population larger than 10
 
 Query the total population of all cities in CITY where District is California.
     SELECT SUM(POPULATION) FROM CITY WHERE DISTRICT = 'California' ;
+
+Query the average population of all cities in CITY where District is California.
+    SELECT AVG(POPULATION) FROM CITY WHERE DISTRICT = 'California';
