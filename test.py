6889d8b822cafebc2be96ae802d51dea0ed0f729
Sara Morison <rob.morison32@gmail.com>
2023-08-17 13:30:35 -0400

    SQL aggregation

--- test.py
+++ test.py
@@ -160,3 +160,6 @@ Query the list of CITY names from STATION that do not end with vowels. Your resu
 
 Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
     SELECT DISTINCT CITY FROM STATION WHERE LEFT (CITY, 1) NOT IN ('A', 'E', 'I', 'O', 'U') OR RIGHT (CITY, 1) NOT IN ('a', 'e', 'i', 'o', 'u');
+
+Query a count of the number of cities in CITY having a Population larger than 100,000.
+    SELECT COUNT(NAME) FROM CITY WHERE POPULATION > 100000;
