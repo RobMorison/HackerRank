bdcc858507b7681fed596858a7caaa332c2fb117
Sara Morison <rob.morison32@gmail.com>
2023-08-17 13:39:02 -0400

    SQL Sum

--- test.py
+++ test.py
@@ -163,3 +163,6 @@ Query the list of CITY names from STATION that either do not start with vowels o
 
 Query a count of the number of cities in CITY having a Population larger than 100,000.
     SELECT COUNT(NAME) FROM CITY WHERE POPULATION > 100000;
+
+Query the total population of all cities in CITY where District is California.
+    SELECT SUM(POPULATION) FROM CITY WHERE DISTRICT = 'California' ;
