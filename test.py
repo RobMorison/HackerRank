394b54bc211a71653dcdd3de39e4d5f397e9fa47
Sara Morison <rob.morison32@gmail.com>
2023-08-30 11:37:07 -0400

    Hackerrank

--- test.py
+++ test.py
@@ -123,49 +123,49 @@
 # SELECT (months * salary) as total_earnings FROM EMPLOYEES;
 
 
-# Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. Your result cannot contain duplicates.
-#     select distinct city from station where left (city,1) not in ('a','e','i','o','u') and right(city,1) not in ('a','e','i','o','u')
+Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. Your result cannot contain duplicates.
+    select distinct city from station where left (city,1) not in ('a','e','i','o','u') and right(city,1) not in ('a','e','i','o','u')
  
 
-# Query the Name of any student in STUDENTS who scored higher than 75 Marks. Order your output by the last three characters of each name. 
-# If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
-#     SELECT NAME FROM STUDENTS WHERE MARKS > 75 ORDER BY RIGHT(NAME, 3), ID ASC;
+Query the Name of any student in STUDENTS who scored higher than 75 Marks. Order your output by the last three characters of each name. 
+If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID.
+    SELECT NAME FROM STUDENTS WHERE MARKS > 75 ORDER BY RIGHT(NAME, 3), ID ASC;
 
-# Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
-#     SELECT NAME FROM EMPLOYEE ORDER BY NAME;
+Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+    SELECT NAME FROM EMPLOYEE ORDER BY NAME;
 
-# Write a query that prints a list of employee names (i.e.: the name attribute)
-# for employees in Employee having a salary greater than 2000 per month who have been employees for less than 10 months. Sort your result by ascending employee_id.
-#     SELECT NAME FROM EMPLOYEE WHERE SALARY > 2000 AND MONTHS < 10 ORDER BY EMPLOYEE_ID;
+Write a query that prints a list of employee names (i.e.: the name attribute)
+for employees in Employee having a salary greater than 2000 per month who have been employees for less than 10 months. Sort your result by ascending employee_id.
+    SELECT NAME FROM EMPLOYEE WHERE SALARY > 2000 AND MONTHS < 10 ORDER BY EMPLOYEE_ID;
 
-# Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
-#     SELECT DISTINCT CITY FROM STATION WHERE LEFT (CITY,1) IN ('A','E','I','O','U');
+Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
+    SELECT DISTINCT CITY FROM STATION WHERE LEFT (CITY,1) IN ('A','E','I','O','U');
 
-# Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
-#     SELECT DISTINCT CITY FROM STATION WHERE RIGHT (CITY,1) IN ('a','e','i','o','u');
+Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
+    SELECT DISTINCT CITY FROM STATION WHERE RIGHT (CITY,1) IN ('a','e','i','o','u');
 
-# Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) as both their first and last characters. Your result cannot contain duplicates.
-#     SELECT DISTINCT CITY FROM STATION WHERE LEFT (CITY,1) IN ('A','E','I','O','U') AND RIGHT (CITY,1) IN ('a','e','i','o','u');
+Query the list of CITY names from STATION which have vowels (i.e., a, e, i, o, and u) as both their first and last characters. Your result cannot contain duplicates.
+    SELECT DISTINCT CITY FROM STATION WHERE LEFT (CITY,1) IN ('A','E','I','O','U') AND RIGHT (CITY,1) IN ('a','e','i','o','u');
 
-# Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
-#     SELECT DISTINCT CITY FROM STATION WHERE LEFT (CITY,1) NOT IN ('A','E','I','O','U');
+Query the list of CITY names from STATION that do not start with vowels. Your result cannot contain duplicates.
+    SELECT DISTINCT CITY FROM STATION WHERE LEFT (CITY,1) NOT IN ('A','E','I','O','U');
 
-# Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths (i.e.: number of characters in the name). 
-# If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically.
-#     SELECT CITY, LENGTH(CITY) FROM STATION ORDER BY LENGTH(CITY), CITY LIMIT 1;
-#     SELECT CITY, LENGTH(CITY) FROM STATION ORDER BY LENGTH(CITY) DESC LIMIT 1;
+Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths (i.e.: number of characters in the name). 
+If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically.
+    SELECT CITY, LENGTH(CITY) FROM STATION ORDER BY LENGTH(CITY), CITY LIMIT 1;
+    SELECT CITY, LENGTH(CITY) FROM STATION ORDER BY LENGTH(CITY) DESC LIMIT 1;
 
-# Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.
-#     SELECT DISTINCT CITY FROM STATION WHERE RIGHT (CITY,1) NOT IN ('a','e','i','o','u');
+Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.
+    SELECT DISTINCT CITY FROM STATION WHERE RIGHT (CITY,1) NOT IN ('a','e','i','o','u');
 
-# Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
-#     SELECT DISTINCT CITY FROM STATION WHERE LEFT (CITY, 1) NOT IN ('A', 'E', 'I', 'O', 'U') OR RIGHT (CITY, 1) NOT IN ('a', 'e', 'i', 'o', 'u');
+Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
+    SELECT DISTINCT CITY FROM STATION WHERE LEFT (CITY, 1) NOT IN ('A', 'E', 'I', 'O', 'U') OR RIGHT (CITY, 1) NOT IN ('a', 'e', 'i', 'o', 'u');
 
-# Query a count of the number of cities in CITY having a Population larger than 100,000.
-#     SELECT COUNT(NAME) FROM CITY WHERE POPULATION > 100000;
+Query a count of the number of cities in CITY having a Population larger than 100,000.
+    SELECT COUNT(NAME) FROM CITY WHERE POPULATION > 100000;
 
-# Query the total population of all cities in CITY where District is California.
-#     SELECT SUM(POPULATION) FROM CITY WHERE DISTRICT = 'California' ;
+Query the total population of all cities in CITY where District is California.
+    SELECT SUM(POPULATION) FROM CITY WHERE DISTRICT = 'California' ;
 
-# Query the average population of all cities in CITY where District is California.
-#     SELECT AVG(POPULATION) FROM CITY WHERE DISTRICT = 'California';
+Query the average population of all cities in CITY where District is California.
+    SELECT AVG(POPULATION) FROM CITY WHERE DISTRICT = 'California';
